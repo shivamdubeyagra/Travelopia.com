e568bfeb9024fd4d93dc34451d3302f4e9a8d324
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-03-28 01:17:16 +0530

    project completed

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let ToggleMenu = document.getElementById("toggle-menu");
+let NavMenu = document.querySelector("ul");
+
+
+ToggleMenu.addEventListener("click", () => {
+  NavMenu.classList.toggle("showMenu");
+});
+
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+document.getElementById("click-button").addEventListener("click", openPopup);
